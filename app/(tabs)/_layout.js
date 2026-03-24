ad76c06753f2004743b1562c3baad1269b3a8b7b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 21:49:54 -0300

    Arrumando o erro principal de entrada em paginas erradas

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -17,13 +17,7 @@ export default function Layout() {
       },
       headerShadowVisible: false
       }}>
-      <Tabs.Screen
-        name="pedidos"
-        options={{
-          title: 'Pedidos',
-          tabBarIcon: ({ color }) => <Ionicons name="pizza" size={24} color={color} />,
-        }}
-      />
+      
       <Tabs.Screen
         name="config"
         options={{
